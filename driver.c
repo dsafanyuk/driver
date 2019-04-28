876588a29cc9d5707aef867a71bd0c687d89b231
Dave Safanyuk <dsafanyuk@gmail.com>
2019-04-28 18:29:58 -0500

    Spacing fixes

--- driver.c
+++ driver.c
@@ -38,29 +38,28 @@
 /**********************************************************************/
 /*                         Symbolic Constants                         */
 /**********************************************************************/
-#define CYLINDERS_PER_DISK 40   /* Number of cylinders in a disk   */
-#define TRACKS_PER_CYLINDER 2   /* Number of tracks in a cylinder  */
-#define SECTORS_PER_TRACK 9     /* Number of sectors in a track    */
-#define BYTES_PER_SECTOR 512    /* Number of bytes in a sector     */
-#define SECTORS_PER_BLOCK 2     /* Number of sectors in a block    */
-#define LIST_HEADER 0           /* Linked list header value        */
-#define LIST_TRAILER 361        /* Linked list trailer value       */
-#define HEADER_ALLOC_ERR 1      /* Header memory allocation error  */
-#define TRAILER_ALLOC_ERR 2     /* Trailer memory allocation error */
-#define REQUEST_ALLOC_ERR 3     /* Request memory allocation error */
-#define MAX_REQUEST_NUM 32767   /* Maximum request number allowed  */
-#define MAX_IDLE_REQUESTS 2     /* Maximum idle requests allowed   */
-#define SENSE_CYLINDER 1        /* Sense cylinder code number      */
-#define SEEK_TO_CYLINDER 2      /* Seek to cylinder code number    */
-#define DMA_SETUP 3             /* DMA setup code number           */
-#define START_MOTOR 4           /* Start motor code number         */
-#define STATUS_MOTOR 5          /* Status motor code number        */
-#define READ_DATA 6             /* Read data code number           */
-#define WRITE_DATA 7            /* Write data code number          */
-#define STOP_MOTOR 8            /* Stop motor code number          */
-#define RECALIBRATE 9           /* Recalibrate code number         */
-#define MAX_PENDING_REQUESTS 20 /* Maximum pending requests        */
-                                /* allowed                         */
+#define CYLINDERS_PER_DISK 40   /* Number of cylinders in a disk      */
+#define TRACKS_PER_CYLINDER 2   /* Number of tracks in a cylinder     */
+#define SECTORS_PER_TRACK 9     /* Number of sectors in a track       */
+#define BYTES_PER_SECTOR 512    /* Number of bytes in a sector        */
+#define SECTORS_PER_BLOCK 2     /* Number of sectors in a block       */
+#define LIST_HEADER 0           /* Linked list header value           */
+#define LIST_TRAILER 361        /* Linked list trailer value          */
+#define HEADER_ALLOC_ERR 1      /* Header memory allocation error     */
+#define TRAILER_ALLOC_ERR 2     /* Trailer memory allocation error    */
+#define REQUEST_ALLOC_ERR 3     /* Request memory allocation error    */
+#define MAX_REQUEST_NUM 32767   /* Maximum request number allowed     */
+#define MAX_IDLE_REQUESTS 2     /* Maximum idle requests allowed      */
+#define SENSE_CYLINDER 1        /* Sense cylinder code number         */
+#define SEEK_TO_CYLINDER 2      /* Seek to cylinder code number       */
+#define DMA_SETUP 3             /* DMA setup code number              */
+#define START_MOTOR 4           /* Start motor code number            */
+#define STATUS_MOTOR 5          /* Status motor code number           */
+#define READ_DATA 6             /* Read data code number              */
+#define WRITE_DATA 7            /* Write data code number             */
+#define STOP_MOTOR 8            /* Stop motor code number             */
+#define RECALIBRATE 9           /* Recalibrate code number            */
+#define MAX_PENDING_REQUESTS 20 /* Maximum pending requests allowed   */
 
 /**********************************************************************/
 /*                         Program Structures                         */
@@ -68,14 +67,14 @@
 /* A file system request list entry                                   */
 struct message
 {
-    int operation_code;                /* The disk operation to be     */
-                                       /* performed                    */
-    int request_number;                /* A unique request number      */
-    int block_number;                  /* The block number to be read  */
-                                       /* or written                   */
-    int block_size;                    /* The block size in bytes      */
-    unsigned long int *p_data_address; /* Points to the data block in  */
-                                       /* memory                       */
+    int operation_code;                /* The disk operation to be    */
+                                       /* performed                   */
+    int request_number;                /* A unique request number     */
+    int block_number;                  /* The block number to be read */
+                                       /* or written                  */
+    int block_size;                    /* The block size in bytes     */
+    unsigned long int *p_data_address; /* Points to the data block in */
+                                       /* memory                      */
 };
 typedef struct message MESSAGE;
 MESSAGE fs_message[MAX_PENDING_REQUESTS]; /* File system request list  */
@@ -101,59 +100,59 @@ REQUEST *p_pending_request_list; /* Points to the pending request     */
 /*                        Function Prototypes                         */
 /**********************************************************************/
 void convert_block(int block, int *p_cylinder, int *p_sector, int *p_track);
-/* Convert physical block numbers into disk drive cylinder, track, */
-/* and sector numbers                                              */
+/* Convert physical block numbers into disk drive cylinder, track,    */
+/* and sector numbers                                                 */
 REQUEST *create_list();
-/* Create an empty pending request list with header and trailer    */
+/* Create an empty pending request list with header and trailer       */
 REQUEST *create_pending_request(MESSAGE fs_message);
-/* Create a new pending request and return its address             */
+/* Create a new pending request and return its address                */
 void add_pending_request(REQUEST *p_pending_request_list,
                          REQUEST *p_new_request);
-/* Add a new pending request and put it in order by block number   */
+/* Add a new pending request and put it in order by block number      */
 void remove_pending_request(REQUEST *p_current_request,
                             REQUEST *p_pending_request_list);
-/* Remove the given request from the pending request list          */
+/* Remove the given request from the pending request list             */
 void set_idle_message(MESSAGE *fs_message);
-/* Check for any invalid parameters and return the error code      */
+/* Check for any invalid parameters and return the error code         */
 int get_error_code(REQUEST *p_current_request);
-/* Set an idle message                                             */
+/* Set an idle message                                                */
 
 /**********************************************************************/
 /*                           Main Function                            */
 /**********************************************************************/
 int main()
 {
-    REQUEST *p_current_request;  /* Points to the current request */
-    bool disk_on = false;        /* Disk drive status             */
-    int error_code = 0,          /* Error code number             */
-        count_fs_message = 0,    /* Count file system request     */
-                                 /* list entries                  */
-        count_idle = 0,          /* Count idle requests           */
-        disk_heads,              /* Current disk heads' position  */
-                                 /* in cylinder number            */
-        cylinder,                /* Cylinder number               */
-        last_request_number = 0, /* Last request number from the  */
-                                 /* file system                   */
-        sector,                  /* Sector number                 */
-        track,                   /* Track number                  */
-        value_sign = 1;          /* Multiplier to change sign     */
+    REQUEST *p_current_request;  /* Points to the current request     */
+    bool disk_on = false;        /* Disk drive status                 */
+    int error_code = 0,          /* Error code number                 */
+        count_fs_message = 0,    /* Count file system request         */
+                                 /* list entries                      */
+        count_idle = 0,          /* Count idle requests               */
+        disk_heads,              /* Current disk heads' position      */
+                                 /* in cylinder number                */
+        cylinder,                /* Cylinder number                   */
+        last_request_number = 0, /* Last request number from the      */
+                                 /* file system                       */
+        sector,                  /* Sector number                     */
+        track,                   /* Track number                      */
+        value_sign = 1;          /* Multiplier to change sign         */
 
-    /* Create empty pending request list                               */
+    /* Create empty pending request list                              */
     p_pending_request_list = create_list();
 
-    /* Loop processing the driver, never stops                         */
+    /* Loop processing the driver, never stops                        */
     while (true)
     {
-        /* Loop processing file system messages                         */
+        /* Loop processing file system messages                       */
         while (fs_message[count_fs_message].operation_code != 0 &&
                count_fs_message < MAX_PENDING_REQUESTS)
         {
-            /* Set last request number to zero everytime it reaches      */
-            /* maximum request number                                    */
+            /* Set last request number to zero everytime it reaches   */
+            /* maximum request number                                 */
             if (last_request_number == MAX_REQUEST_NUM)
                 last_request_number = 0;
 
-            /* Add a request into pending request list                   */
+            /* Add a request into pending request list                */
             if (fs_message[count_fs_message].request_number > last_request_number ||
                 fs_message[count_fs_message].request_number <= 0)
             {
@@ -171,17 +170,17 @@ int main()
         }
         count_fs_message = 0;
 
-        /* Check if pending request list have any request to process    */
+        /* Check if pending request list have any request to process  */
         if (p_pending_request_list->p_next_request->p_next_request != NULL)
         {
-            /* Turn the disk drive motor on if it is off                 */
+            /* Turn the disk drive motor on if it is off              */
             count_idle = 0;
             if (disk_on == false)
             {
                 disk_on = disk_drive(START_MOTOR, 0, 0, 0, 0);
 
-                /* Loop with empty body, wait until the disk motor is up  */
-                /* to speed                                               */
+                /* Loop with empty body, wait until the disk motor is */
+                /* up to speed                                        */
                 while (disk_drive(STATUS_MOTOR, 0, 0, 0, 0) != 0)
                     ;
 
@@ -189,8 +188,8 @@ int main()
                 disk_heads = disk_drive(SENSE_CYLINDER, 0, 0, 0, 0);
             }
 
-            /* Choose the next request to process using a disk arm       */
-            /* elevator scheduling algorithm                             */
+            /* Choose the next request to process using a disk arm    */
+            /* elevator scheduling algorithm                          */
             p_current_request = p_pending_request_list->p_next_request;
             while (convert_block(p_current_request->block_number, &cylinder,
                                  &sector, &track),
@@ -208,27 +207,28 @@ int main()
                 }
             }
 
-            /* Change the multiplier to positive                         */
+            /* Change the multiplier to positive                      */
             if (value_sign == -1)
                 value_sign = 1;
 
-            /* Get and set the error code                                */
+            /* Get and set the error code                             */
             error_code = get_error_code(p_current_request);
 
-            /* Process the request if there is no errors                 */
+            /* Process the request if there is no errors              */
             if (error_code == 0)
             {
                 /* Check if the request to process is in another cylinder */
                 if (disk_heads != cylinder)
                 {
-                    /* Send the disk heads to the given cylinder           */
-                    disk_heads = disk_drive(SEEK_TO_CYLINDER, cylinder, 0, 0, 0);
+                    /* Send the disk heads to the given cylinder      */
+                    disk_heads = disk_drive(SEEK_TO_CYLINDER, 
+                                                     cylinder, 0, 0, 0);
 
-                    /* Check if the disk heads land correctly              */
+                    /* Check if the disk heads land correctly         */
                     while (disk_heads != cylinder)
                     {
-                        /* Loop with empty body, wait until the disk heads  */
-                        /* land on cylinder zero                            */
+                        /* Loop with empty body, wait until the disk  */
+                        /* heads land on cylinder zero                */
                         while (disk_drive(RECALIBRATE, 0, 0, 0, 0) != 0)
                             ;
 
@@ -237,17 +237,18 @@ int main()
                         if (cylinder != 0)
                         {
                             disk_heads =
-                                disk_drive(SEEK_TO_CYLINDER, cylinder, 0, 0, 0);
+                                disk_drive(SEEK_TO_CYLINDER, 
+                                                    cylinder, 0, 0, 0);
                         }
                     }
                 }
 
-                /* Process the request if DMA sets up correctly           */
-                if (disk_drive(DMA_SETUP, sector, track, BYTES_PER_SECTOR *
-                               SECTORS_PER_BLOCK, 
+                /* Process the request if DMA sets up correctly       */
+                if (disk_drive(DMA_SETUP, sector, track, 
+                               BYTES_PER_SECTOR * SECTORS_PER_BLOCK, 
                                p_current_request->p_data_address) == 0)
                 {
-                    /* Read or write the request                           */
+                    /* Read or write the request                      */
                     if (p_current_request->operation_code == 1)
                     {
                         while (disk_drive(READ_DATA, 0, 0, 0, 0) != 0)
@@ -266,25 +267,25 @@ int main()
             }
             else
             {
-                /* Set the error code to be send to the file system       */
+                /* Set the error code to be send to the file system   */
                 fs_message[0].operation_code = error_code;
                 error_code = 0;
             }
 
-            /* Set a message and send it to the file system              */
+            /* Set a message and send it to the file system           */
             fs_message[0].request_number = p_current_request->request_number;
             fs_message[0].p_data_address = p_current_request->p_data_address;
             fs_message[0].block_number = p_current_request->block_number;
             fs_message[0].block_size = p_current_request->block_size;
             send_message(fs_message);
 
-            /* Remove the request from the pending request list          */
+            /* Remove the request from the pending request list       */
             remove_pending_request(p_current_request, p_pending_request_list);
         }
         else
         {
-            /* Turn the disk drive motor off after receiving two idle    */
-            /* request in a row from the file system                     */
+            /* Turn the disk drive motor off after receiving two idle */
+            /* request in a row from the file system                  */
             count_idle += 1;
             if (count_idle >= MAX_IDLE_REQUESTS && disk_on)
             {
@@ -292,7 +293,7 @@ int main()
                 disk_on = false;
             }
 
-            /* Set an idle message and send it to the file system        */
+            /* Set an idle message and send it to the file system     */
             set_idle_message(&fs_message[0]);
             send_message(fs_message);
         }
@@ -328,7 +329,7 @@ void convert_block(int block, int *p_cylinder, int *p_sector, int *p_track)
 /**********************************************************************/
 REQUEST *create_list()
 {
-    REQUEST *p_new_list; /* Points to the new pending request list     */
+    REQUEST *p_new_list; /* Points to the new pending request list    */
 
     if ((p_new_list = (REQUEST *)malloc(sizeof(REQUEST))) == NULL)
     {
@@ -359,7 +360,7 @@ REQUEST *create_list()
 /**********************************************************************/
 REQUEST *create_pending_request(MESSAGE fs_message)
 {
-    REQUEST *p_new_request; /* Points to the new request               */
+    REQUEST *p_new_request; /* Points to the new request              */
 
     if ((p_new_request = (REQUEST *)malloc(sizeof(REQUEST))) == NULL)
     {
@@ -422,7 +423,7 @@ void remove_pending_request(REQUEST *p_current_request,
 /**********************************************************************/
 int get_error_code(REQUEST *p_current)
 {
-    int error_code = 0; /* The error code to be returned               */
+    int error_code = 0; /* The error code to be returned              */
 
     if (p_current->operation_code != 1 &&
         p_current->operation_code != 2)
